6dbfa4e65444e9d7324f57ce350178e91f884f1b
henrique.dias <henrique.dias@feedzai.com>
2019-10-07 14:47:35 +0100

    minor fix

--- index.js
+++ index.js
@@ -257,9 +257,7 @@ function getRepoFolder(repo) {
         return process.cwd();
     }
 
-    let folder = path.join(__dirname, "../tmp");
-
-    return folder;
+    return "/tmp/repo-analyzer";
 }
 
 /**
